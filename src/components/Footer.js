297afe2a6ce77bf42bca1a9e1cff122f46e4e257
Your Name <JulienneJulien15@gmail.com>
2023-04-06 00:31:59 -0400

    added javascript to auto update footer copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div>
+
+        </div>
+        <p>JayJae &#128156; Coding!</p>
+        <p className='footnote'>
+            Copyright &copy; <span id='cyear'></span> by Julienne Julien. All Rights Reserved.
+        </p>
+    </div>
+    
   )
 }
-
+// JAVASCRIPT to AUTO UPDATE current year copyright
+window.addEventListener('load', () => {
+    const year = document.getElementById('cyear');
+    year.innerHTML = new Date().getFullYear();
+});
 export default Footer
