a4bafa3ccbb35fbb20b2e248f71264f2a712e59d
Your Name <JulienneJulien15@gmail.com>
2023-04-06 19:51:20 -0400

    added about me and profilepic

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,8 +10,8 @@ function Navbar() {
         <button> </button>
     </div>
     <div className='links'>
-        <Link to="/Home"> Home </Link>
-        <Link to="/About"> About </Link>
+        <Link to="/"> Home </Link>
+        <Link to="/"> About </Link>
         <Link to="/Portfolio"> Portfolio </Link>
         <Link to="/Contact"> Contact </Link>
         <Link to="/Resume"> Resume </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import profilepic from '..//images/profile.jpg';
+
 
 function Home() {
   return (
@@ -6,6 +8,14 @@ function Home() {
       <div className='intro'>
           <h1> Welcome! &#128075;&#127999; I'm Julienne</h1>
             <h2> There is always one more bug to fix. – Ellen Ullman </h2>
+
+              <div>
+                <img src={profilepic} alt="portfolio_profile_picture"/> 
+              </div>
+
+            <div className='aboutMe'>
+              <h2>The full-stack web development boot camp I am attending makes me passionate about gaining new knowledge and growing as a developer. Front-end web development appeals to me more because it appeals to all my passions. My creativity, curiosity, and problem-solving abilities are all incorporated into it. My goal is to continue learning and growing as a developer, and I strive to put my knowledge to work whenever possible.</h2>
+            </div>
       </div>
     </div>
   )
