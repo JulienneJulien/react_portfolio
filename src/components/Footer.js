2d51cbbd44a5d32d6bf3ad146504883aee2d554a
Your Name <JulienneJulien15@gmail.com>
2023-04-06 16:55:02 -0400

    installed material icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { GitHub, LinkedIn } from '@mui/icons-material';
+
 
 function Footer() {
   return (
     <div className='footer'>
         <div>
-        
+            
+        {/* <FontAwesomeIcon icon={faEnvelope} /> */}
+        <GitHub/>
+        <LinkedIn/>
         </div>
         <p>JayJae &#128156; Coding!</p>
         <p className='footnote'>
