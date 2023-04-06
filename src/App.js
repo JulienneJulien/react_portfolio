b5190b76d943365ad6a3d5175a35d2c08ae2c177
Your Name <JulienneJulien15@gmail.com>
2023-04-05 22:01:21 -0400

    imported individual components to be rendered

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 // This enables routing in our REACT APP
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 // EXPLANATION: This should hold everything in our app that requires routing. That is, if we need routing throughout our app, we must wrap our higher component in BrowserRouter.
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
     <div className="App">
      <Router> 
        <Routes>
-        <route path="/" element={<About />} />
-        <route path="/" element={<Portfolio />} />
-        <route path="/" element={<Contact />} />
-        <route path="/" element={<Resume />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Resume" element={<Resume />} />
        </Routes>
      </Router>
     </div>
