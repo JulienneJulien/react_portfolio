b9af2fc89a54ed02dfec33f547f5ab350d70a609
Your Name <JulienneJulien15@gmail.com>
2023-04-10 19:54:41 -0400

    initial resume code and styling was added

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement  } from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css";
+
 
 function Resume() {
   return (
-    <div>Resume</div>
+  <div className='Resume'>
+     <h2>Resume</h2> 
+
+    <div className='experience'> 
+    <VerticalTimeline lineColor='#413C58'>
+      <VerticalTimelineElement className='vertical-timeline--element--eduation' 
+        date=''
+        iconStyle={{backgroud: '#413C58'}}
+      ></VerticalTimelineElement>
+      </VerticalTimeline>
+      </div>
+  </div>
+  
+   
   )
 }
 
-export default Resume
+export default Resume;
