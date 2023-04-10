57385c083ca31cabb2ae3a4dbbd35cccb68eb8e2
Your Name <JulienneJulien15@gmail.com>
2023-04-10 17:07:57 -0400

    added initial contact form code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function Contact() {
+
+    const [formState, setFormState] = useState({name: '', email:'', message:''});
+
+    const [error, setErrorMessage] = useState('');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>Contact</div>
   )
 }
 
 export default Contact
+
+
