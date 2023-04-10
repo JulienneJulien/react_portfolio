57004c0ae1a39ed6f7d6040faf9e320287711153
Your Name <JulienneJulien15@gmail.com>
2023-04-09 22:33:36 -0400

    Created an array of objects to loop through list of projects and display them

---
+++ src/helpers/PortfolioCardsList.js
@@ -0,0 +1,33 @@
+import profile from '../images/profile.jpg'
+
+
+export const portfolioCards = [
+
+{
+   title: 'test1' ,
+   image: profile
+},
+{
+    title: 'test2' ,
+    image: profile
+ },
+
+ {
+    title: 'test3' ,
+    image: profile
+ },
+
+ {
+    title: 'test4' ,
+    image: profile
+ },
+ {
+    title: 'test5' ,
+    image: profile
+ },
+ {
+    title: 'test6' ,
+    image: profile
+ },
+  
+]
