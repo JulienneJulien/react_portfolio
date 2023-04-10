d657368071cb4fd4fa0218f2ee699c548ccd4057
Your Name <JulienneJulien15@gmail.com>
2023-04-09 21:49:34 -0400

    Created Portfolio css file and initial styling added

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import ProjectCards from '../components/ProjectCards'
+import profile from '../images/profile.jpg'
+import "../styles/Portfolio.css"
 
 function Portfolio() {
   return (
     <div className='portfolio'>
       <h1>My Projects</h1>
       <div className='portfolioCards'></div>
+        <ProjectCards title= 'test1' image={profile}/>
+        <ProjectCards title= 'test2' image={profile}/>
+        <ProjectCards title= 'test3' image={profile}/>
+        <ProjectCards title= 'test4' image={profile}/>
+        <ProjectCards title= 'test5' image={profile}/>
+        <ProjectCards title= 'test6' image={profile}/>
     </div>
   )
 }
